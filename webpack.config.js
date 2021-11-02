5260d92860cfa4f04f57a075e44e18841918c8ae
James <admin@floretos.com>
2021-11-02 18:27:18 +0700

    fix: build size improvement

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
 const path = require('path')
 
-const prod = process.env.NODE_ENV === 'production'
+const prod = true
 
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpackMockServer = require('webpack-mock-server')
+const TerserPlugin = require('terser-webpack-plugin')
 
 require('dotenv').config()
 
 const configName = process.env.APP_CONFIG || process.env.NODE_ENV || 'development'
 
 module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: 4,
+        terserOptions: {
+          ecma: undefined,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          module: false,
+          sourceMap: false,
+        },
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+    },
+  },
   mode: prod ? 'production' : 'development',
   entry: './src/index.tsx',
   devServer: {
