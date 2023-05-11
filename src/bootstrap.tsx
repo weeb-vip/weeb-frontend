68eb9ebddcfbd88f70dfef4475573b7be9048e95
James <james@honestbank.com>
2023-05-11 19:58:04 +0700

    feat: algolia (#21)

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,5 +1,7 @@
 import React, {useState, Suspense, useEffect} from 'react'
 import configApi from './services/api/config'
+import flagsmith from "flagsmith";
+import {FlagsmithProvider} from "flagsmith/react";
 
 const Bootstrap = () => {
   const [loaded, setLoaded] = useState(false)
@@ -16,9 +18,15 @@ const Bootstrap = () => {
     const QueryProvider = React.lazy(() => import('./queryProvider'))
     return (
       <Suspense fallback={<div>Loading...</div>}>
-        <QueryProvider>
-          <App/>
-        </QueryProvider>
+        <FlagsmithProvider options={{
+          // @ts-ignore
+          environmentID: global.config.flagsmith_environment_id,
+          api: "https://flagsmith.weeb.vip/api/v1/",
+        }} flagsmith={flagsmith}>
+          <QueryProvider>
+            <App/>
+          </QueryProvider>
+        </FlagsmithProvider>
       </Suspense>
     )
   }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,29 @@
 import Button, {ButtonColor} from "../Button";
 import Search from "../Search";
-import { searchResult} from "../../services/api/search";
+import {searchResult} from "../../services/api/search";
 import api from "../../services/api";
 import {Link, useNavigate} from "react-router-dom";
+import {useFlags} from "flagsmith/react";
+import {autocomplete, getAlgoliaResults} from '@algolia/autocomplete-js';
+import {
+  InstantSearch,
+  Configure,
+  Hits,
+  SearchBox,
+  Panel,
+  RefinementList,
+  Pagination,
+  Highlight, connectSearchBox, Snippet,
+} from 'react-instantsearch-dom';
+import algoliasearch from "algoliasearch/lite";
+import React, { useState} from "react";
+import Autocomplete from "../Autocomplete";
 
 function Header() {
+
+  const flags = useFlags(['algolia_search']);
   const navigate = useNavigate()
+  // @ts-ignore
   return (
     <>
       <div className="flex flex-row items-center justify-between p-4 bg-white-800 border-b border-gray-200">
@@ -13,37 +31,55 @@ function Header() {
           <Link to={"/"}>
             <div className="flex flex-row items-center justify-center bg-gray-200"
                  style={{width: '112px', height: '40px'}}>
-              <img src="/logo192.png" alt="logo" style={{width: '112', height: '40px'}}/>
+              <img src="" alt="logo" style={{width: '112', height: '40px'}}/>
             </div>
           </Link>
           <span className="text-xl font-normal">Anime</span>
         </div>
         <div className="flex flex-row items-center space-x-4">
-          <Search<searchResult>
-            searchFunction={api.search.search}
-            className="relative w-72 z-10"
-            parseSearchResult={(item: searchResult) => item.name}
-            mapFunction={
-              (selectItem) => (item: searchResult) => (
-                <div onClick={() => {
-                  selectItem(item)
-                  navigate(`/show/${item.type}/${item.anidbid}`)
-                }}
-                     className="flex flex-row items-center space-x-2 p-2"
-                >
-                  <img
-                    src={`${(global as any).config.api_host}/show/anime/${item.id.split('-')[0].toLowerCase()}/${item.id.replace(/[^0-9.]/gm, '')}/poster`}
-                    alt={item.name}
-                    style={{height: '50px'}}
-                    className={"aspect-2/3 m-2"}
-                  />
-                  <div className={"flex flex-col flex-shrink"}>
-                    <span>{item.name}</span>
-                    <span>{item.year}</span>
+          {!flags.algolia_search.enabled ? (
+            <Search<searchResult>
+              searchFunction={api.search.search}
+              className="relative w-72 z-10"
+              parseSearchResult={(item: searchResult) => item.name}
+              mapFunction={
+                (selectItem) => (item: searchResult) => (
+                  <div onClick={() => {
+                    selectItem(item)
+                    navigate(`/show/${item.type}/${item.anidbid}`)
+                  }}
+                       className="flex flex-row items-center space-x-2 p-2"
+                  >
+                    <img
+                      src={`${(global as any).config.api_host}/show/anime/${item.id.split('-')[0].toLowerCase()}/${item.id.replace(/[^0-9.]/gm, '')}/poster`}
+                      alt={item.name}
+                      style={{height: '50px'}}
+                      className={"aspect-2/3 m-2"}
+                    />
+                    <div className={"flex flex-col flex-shrink"}>
+                      <span>{item.name}</span>
+                      <span>{item.year}</span>
+                    </div>
                   </div>
-                </div>
-              )
-            }/>
+                )
+              }/>
+          ) : (
+            <div className={'relative'}>
+              <Autocomplete
+
+              />
+            </div>
+
+
+            //   <InstantSearch searchClient={searchClient} indexName="instant_search">
+            //   <SearchBox
+            //     className="searchbox"
+            //     translations={{
+            //       placeholder: '',
+            //     }}
+            //   />
+            // </InstantSearch>
+          )}
           <Button color={ButtonColor.blue} showLabel={true} label={"Login"} onClick={() => {
           }} icon={null}/>
           <Button color={ButtonColor.transparent} showLabel={true} label={"Register"} onClick={() => {
@@ -53,5 +89,6 @@ function Header() {
     </>
   )
 }
+
 
 export {Header as default}
