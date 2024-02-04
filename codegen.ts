784c55d310b940a59e721ab072034b8f02694e21
James <james@honestbank.com>
2024-02-03 23:23:52 -0500

    fix: episodes type (#24)

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,8 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: './schema.graphql',
+  // schema: './schema.graphql',
+  schema: "https://gateway.staging.weeb.vip/graphql",
   documents: ['src/**/*.tsx', "src/**/*.ts"],
   generates: {
     './src/gql/': {
