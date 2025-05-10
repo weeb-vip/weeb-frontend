e5abae1899f8416abae59bbc6360e09785552ded
James <admin@floretos.com>
2025-05-10 19:40:37 -0400

    fix: loader size

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
   // circular progress
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center">
       {/*<div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>*/}
       <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
