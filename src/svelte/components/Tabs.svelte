996fa8cbbbdb0d69ce31831033812cfa0d23622f
James Trotter <james.trotter@joingotu.com>
2025-10-19 13:04:37 -0400

    fix: dropdown on mobile

--- src/svelte/components/Tabs.svelte
+++ src/svelte/components/Tabs.svelte
@@ -265,7 +265,7 @@
     bind:this={tabsRef}
     use:assignStickyRef
     class="sm:hidden transition-all duration-200 {isSticky
-      ? 'fixed left-0 right-0 z-30 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 px-4 py-2 pt-6 shadow-sm'
+      ? 'fixed left-0 right-0 z-10 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 px-4 py-2 pt-6 shadow-sm'
       : ''}"
     style={isSticky ? 'top: 10.5rem' : ''}
   >
